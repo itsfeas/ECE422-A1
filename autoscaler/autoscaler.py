762ae8ee1b4cdaf22e4525527f3da3e1efe42663
itsfeas <amf.fiaz@gmail.com>
2024-01-18 15:58:54 -0700

    autoscaler init

--- autoscaler/autoscaler.py
+++ autoscaler/autoscaler.py
@@ -1,7 +1,10 @@
 import docker
 
-def scale_up():
-    pass
+def scale_up(client: docker.DockerClient):
+    service = client.services.get("web")
+    n_replicas = client
+    client.api.get
+    client.services.model.scale(replicas=n_replicas+1)
 
 def scale_down():
     pass
@@ -11,3 +14,7 @@ def monitor():
 
 if __name__ == "__main__":
     client = docker.from_env()
+    # api_client = docker.APIClient(base_url='unix://var/run/docker.sock')
+    model = client.services.create(image="zhijiewang22/simpleweb")
+    
+    
