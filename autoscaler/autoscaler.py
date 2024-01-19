8cff2001981507639e97a0f37a20c54eb860ff2b
itsfeas <amf.fiaz@gmail.com>
2024-01-19 12:07:18 -0700

    scale to the mooooooooooon

--- autoscaler/autoscaler.py
+++ autoscaler/autoscaler.py
@@ -2,8 +2,10 @@ import time
 import docker
 import redis
 
+SERVICE_NAME = "simpleweb_web"
+
 def get_replicas(api_client: docker.APIClient):
-    conf_dic = api_client.inspect_service("simpleweb")
+    conf_dic = api_client.inspect_service(SERVICE_NAME)
     try:
         n_replicas = conf_dic["Spec"]["Mode"]["Replicated"]["Replicas"]
     except:
@@ -11,7 +13,7 @@ def get_replicas(api_client: docker.APIClient):
     return n_replicas
 
 def scale_up(api_client: docker.APIClient, model):
-    conf_dic = api_client.inspect_service("simpleweb")
+    conf_dic = api_client.inspect_service(SERVICE_NAME)
     # n_replicas = None
     # try:
     #     n_replicas = conf_dic["Spec"]["Mode"]["Replicated"]["Replicas"]
@@ -20,19 +22,20 @@ def scale_up(api_client: docker.APIClient, model):
     n_replicas = get_replicas(api_client)
     model.scale(replicas=n_replicas+1)
 
-def scale_down(api_client: docker.APIClient):
+def scale_down(api_client: docker.APIClient, model):
     n_replicas = get_replicas(api_client)
     model.scale(replicas=n_replicas-1)
 
-def get_response_times():
-    l = red.llen("client:times")
-    lst = [int(t) for t in red.lpop("client:times", l)]
-    return sum(lst)/len(lst)
+def get_hits(red: redis.Redis):
+    i = red.get("hits")
+    i = i if i else 0
+    red.set("hits", 0)
+    return i
 
 def init_service(client: docker.DockerClient):
     model = client.services.create(
         image="zhijiewang22/simpleweb:1",
-        name="simpleweb",
+        name=SERVICE_NAME,
         resources = {
             "limits": {
                 "cpus": "0.25",
@@ -53,15 +56,20 @@ def init_service(client: docker.DockerClient):
 if __name__ == "__main__":
     client = docker.DockerClient(base_url='unix://var/run/docker.sock')
     api_client = docker.APIClient(base_url='unix://var/run/docker.sock')
-    api_client.remove_service("simpleweb")
+    # api_client.remove_service("simpleweb")
 
-    model = init_service(client)
+    # model = init_service(client)
+    services = client.services.list(filters = { "name": SERVICE_NAME })
+    model = services[0]
+    model = client.services.get(model.id)
     red = redis.Redis(host='localhost', port=6379)
 
     while True:
         time.sleep(20)
-        ave = get_response_times()
-        if ave>20:
+        hits = get_hits(red)
+        replicas = get_replicas(api_client)
+        ratio = (hits/20)/replicas #(hits/s)/replica
+        if replicas==0 or ratio>5:
             scale_up(api_client, model)
-        elif ave<5:
+        elif hits<1 and replicas>1:
             scale_down(api_client, model)
