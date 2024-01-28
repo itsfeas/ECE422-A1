965ecc5243dda0f0c967998783be2bbb6e03806c
itsfeas <amf.fiaz@gmail.com>
2024-01-28 15:48:18 -0700

    autoscaler ---> oop

--- autoscaler/autoscaler.py
+++ autoscaler/autoscaler.py
@@ -4,52 +4,77 @@ import redis
 
 SERVICE_NAME = "simpleweb_web"
 
-def get_replicas(api_client):
-    conf_dic = api_client.inspect_service(SERVICE_NAME)
-    try:
-        n_replicas = conf_dic["Spec"]["Mode"]["Replicated"]["Replicas"]
-    except:
-        print("SERVICE NOT CONFIGURED PROPERLY!!!!")
-    return n_replicas
-
-def scale_up(api_client, model, ratio):
-    conf_dic = api_client.inspect_service(SERVICE_NAME)
-    n_replicas = get_replicas(api_client)
-    model.scale(replicas=n_replicas+1+(ratio-5))
-
-def scale_down(api_client, model):
-    n_replicas = get_replicas(api_client)
-    model.scale(replicas=n_replicas-1)
+class RedisClient:
+    red = None
+    def __init__(self, host='localhost', port=6379):
+        self.red = redis.Redis(host=host, port=port)
     
+    def get_hits(self):
+        i = self.red.get("hits")
+        return i if i else 0
+    
+    def reset_hits(self):
+        self.red.set("hits", 0)
 
-def get_hits(red):
-    i = red.get("hits")
-    i = i if i else 0
-    red.set("hits", 0)
-    return int(i)
+
+class AutoScaler:
+    client = None
+    api_client = None
+    model = None
+    limit = None
+    red = None
+    def __init__(self, limit=50):
+        self.client = docker.DockerClient(base_url='unix://var/run/docker.sock')
+        self.api_client = docker.APIClient(base_url='unix://var/run/docker.sock')
+        self.red = RedisClient()
+        self.limit = limit
+
+    def get_replicas(self):
+        conf_dic = self.api_client.inspect_service(SERVICE_NAME)
+        try:
+            n_replicas = conf_dic["Spec"]["Mode"]["Replicated"]["Replicas"]
+        except:
+            print("SERVICE NOT CONFIGURED PROPERLY!!!!")
+        return n_replicas
+    
+    def scale_up(self, ratio):
+        n_replicas = self.get_replicas()
+        self.model.scale(replicas=n_replicas+1+(ratio-5))
+
+    def scale_down(self):
+        n_replicas = self.get_replicas()
+        self.model.scale(replicas=n_replicas-1)
+    
+    def connect(self):
+        services = self.client.services.list(filters = { "name": SERVICE_NAME })
+        m = services[0]
+        self.model = self.client.services.get(m.id)
+    
+    def get_hits(self):
+        hits = self.red.get_hits()
+        self.red.reset_hits()
+        return int(hits)
+    
+    def monitor(self, interval):
+        while True:
+            # update model of service
+            self.connect()
+
+            hits = self.get_hits()
+            replicas = self.get_replicas()
+            ratio = (hits*2/replicas) if replicas != 0 else 1
+
+            print("hits: ", hits, "ratio: ", ratio, "replicas: ", replicas)
+            if replicas==0 or (ratio>5 and replicas<self.limit):
+                self.scale_up(ratio)
+            elif ratio<2 and replicas>1:
+                self.scale_down()
+            time.sleep(interval)
+
 
 if __name__ == "__main__":
-    client = docker.DockerClient(base_url='unix://var/run/docker.sock')
-    api_client = docker.APIClient(base_url='unix://var/run/docker.sock')
-    # api_client.remove_service("simpleweb")
-
-    # model = init_service(client)
-    red = redis.Redis(host='localhost', port=6379)
-
     interval = 10
-    while True:
-        # update model of service
-        services = client.services.list(filters = { "name": SERVICE_NAME })
-        model = services[0]
-        model = client.services.get(model.id)
+    red = redis.Redis(host='localhost', port=6379)
+    scaler = AutoScaler()
+    scaler.monitor(interval)
 
-        hits = get_hits(red)
-        replicas = get_replicas(api_client)
-        ratio = (hits*2/replicas) if replicas != 0 else 1
-
-        print("hits: ", hits, "ratio: ", ratio, "replicas: ", replicas)
-        if replicas==0 or (ratio>5 and replicas<50):
-            scale_up(api_client, model, ratio)
-        elif ratio<2 and replicas>1:
-            scale_down(api_client, model)
-        time.sleep(interval)
